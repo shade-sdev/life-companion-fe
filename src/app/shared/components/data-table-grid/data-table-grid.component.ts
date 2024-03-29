9f967a79ebc7408be0e95fa5c5d6cc157f0e8b37
Shade <Shade@Shade.ga>
2024-03-29 08:19:18 +0400

    data table

--- src/app/shared/components/data-table-grid/data-table-grid.component.ts
+++ src/app/shared/components/data-table-grid/data-table-grid.component.ts
@@ -1,11 +1,14 @@
 import {Component} from '@angular/core';
 import {NgIcon} from "@ng-icons/core";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-data-table-grid',
   standalone: true,
   imports: [
-    NgIcon
+    NgIcon,
+    FormsModule,
+    ReactiveFormsModule
   ],
   templateUrl: './data-table-grid.component.html',
   styleUrl: './data-table-grid.component.css'
