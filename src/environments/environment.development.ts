0f8c6a8cb473b34f86cd7e9609c6c5079f290c8c
Shade <Shade@Shade.ga>
2024-04-05 19:24:58 +0400

    table

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
+  production: false,
   basePath: "http://localhost:8080",
   baseApiPath: "http://localhost:8080/api/v1"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
+  production: true,
   basePath: "https://test-bench.onrender.com",
   baseApiPath: "https://test-bench.onrender.com/api/v1"
 };
