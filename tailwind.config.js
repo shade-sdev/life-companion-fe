005f9032089e9da3e855a832c6b84e190fedb19b
Shade <Shade@Shade.ga>
2024-04-06 16:24:48 +0400

    table

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'life-100-dark': '#0b0b0e',
+        'life-200-dark': '#111214',
+        'life-300-dark': '#1b1c20',
+        'life-border-dark': '#1b1c20'
+      }
+    },
   },
   plugins: [],
 }
