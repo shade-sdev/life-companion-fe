51b889427d1e3dc2469664ceb3012cc76a601009
Shade <Shade@Shade.ga>
2024-04-09 22:20:52 +0400

    file upload/download

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  basePath: "http://165.232.173.128:8080/",
-  baseApiPath: "http://165.232.173.128:8080/api/v1"
+  basePath: "https://165.232.173.128/",
+  baseApiPath: "https://165.232.173.128/api/v1"
 };
